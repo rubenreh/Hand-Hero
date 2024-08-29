a169c90441dd6630e20f4a54a31c01e27ee0db29
Ruben Rehal <149988511+rubenreh@users.noreply.github.com>
2024-08-29 11:09:37 -0400

    Create GestureCalibration.h

---
+++ GestureCalibration.h
@@ -0,0 +1,11 @@
+#ifndef GESTURE_CALIBRATION_H
+#define GESTURE_CALIBRATION_H
+
+// GestureCalibration class to handle calibration of gesture recognition
+class GestureCalibration {
+public:
+    // Method to calibrate gesture recognition
+    void calibrate();
+};
+
+#endif
